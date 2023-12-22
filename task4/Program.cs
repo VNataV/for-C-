6b012ec0d1d19c06edebe036c9dcadd6b9a14062
Natalja Veshcheva <rod2vek@gmail.com>
2023-12-22 22:41:21 +0300

    lesson1:task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 10;
+int c = 15;
+int d = 3;
+int e = 8;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
